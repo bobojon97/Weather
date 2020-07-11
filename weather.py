eb9821785d80d6ac268ef2e7765ce52e4ffc8aa5
bobojon <rbobojon1@gmail.com>
2020-07-11 17:49:09 +0500

    first commit

---
+++ weather.py
@@ -0,0 +1,25 @@
+import pyowm
+
+owm = pyowm.OWM('babbb8bb07c8f901a3e17f6337ca300a', {'language': 'ru'})
+
+place = input('В каком городе/стране ?: ')
+
+wer = owm.weather_manager().weather_at_place(place)
+w = wer.get_weather()
+
+temp = w.get_temperature('celsius')['temp']
+
+print ('В городе ' , place , 'сейчас', w.get_detailed_status())
+
+print ('Температура сейчас в районе ',str(temp))
+
+if temp < 10:
+	print('Сейчас ппц как холодно одевайся как танк !')
+
+
+elif temp < 20:
+	print('Сейчас холодно, оденься потеплее')
+	
+
+else:
+	print('Температура норм, одевайся что  угодно')
